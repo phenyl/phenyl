13d14d4a58c03e5daee7b8082c3044ee1d542ec8
Shin Suzuki <shinout310@gmail.com>
2017-08-13 19:13:51 +0900

    [WIP] phenyl-state: add index

---
+++ modules/phenyl-state/src/index.js
@@ -0,0 +1,25 @@
+// @flow
+import type {
+  UpdateCommand
+} from 'phenyl-interfaces'
+
+type PlainPhenylState = {
+
+}
+
+/**
+ *
+ */
+
+class PhenylState {
+  constructor(plain: PlainPhenylState) {
+  }
+
+  /**
+   *
+   */
+  $update(command: UpdateCommand): PhenylState {
+  }
+
+
+}
