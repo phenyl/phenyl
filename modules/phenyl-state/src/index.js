e718cc7dd0c16f4077b0e126ae1a57f18ad55088
Shin Suzuki <shinout310@gmail.com>
2017-08-14 01:51:59 +0900

    phenyl-state: [WIP] add interface

--- modules/phenyl-state/src/index.js
+++ modules/phenyl-state/src/index.js
@@ -1,9 +1,17 @@
 // @flow
 import type {
-  UpdateCommand
+  DeleteCommand,
+  IdQuery,
+  IdsQuery,
+  InsertCommand,
+  Restorable,
+  UpdateCommand,
+  WhereQuery,
 } from 'phenyl-interfaces'
 
+import { filter } from 'power-filter/jsnext'
 import { assign } from 'power-assign/jsnext'
+import EntityPool from './entity-pool'
 
 type PlainPhenylState = {
 
@@ -13,7 +21,30 @@ type PlainPhenylState = {
  *
  */
 class PhenylState {
+  entities: { [name: string]: EntityPool }
+
   constructor(plain: PlainPhenylState) {
+  }
+
+  /**
+   *
+   */
+  find(query: WhereQuery): Array<Restorable> {
+    const allEntities = this.entities[query.from].getAll()
+    return filter(allEntities, query.where)
+  }
+
+  /**
+   *
+   */
+  findOne(query: WhereQuery): Restorable {
+    return this.find(query)[0]
+  }
+  /**
+   *
+   */
+  get(query: IdQuery): Restorable {
+    return this.entities[query.from].get(query.id)
   }
 
   /**
@@ -23,5 +54,17 @@ class PhenylState {
     return assign(this, command.operators)
   }
 
+  /**
+   *
+   */
+  $delete(command: DeleteCommand): PhenylState {
+    return assign(this, command.operators)
+  }
 
+  /**
+   *
+   */
+  $insert(command: InsertCommand): PhenylState {
+    return assign(this, command.operators)
+  }
 }
