478802d83d99d543e6defc5cff4eb867a136ec2e
Shin Suzuki <shinout310@gmail.com>
2017-08-21 01:12:19 +0900

    phenyl: add power-crypt

---
+++ modules/power-crypt/src/index.js
@@ -0,0 +1,29 @@
+// @flow
+import shajs from 'sha.js'
+
+type PowerCryptOptions = {
+  nStretch?: number,
+  algorithm?: 'sha224' | 'sha256' | 'sha384' | 'sha384' | 'sha512',
+  encode?: 'hex' | 'utf8' | 'base64',
+  salt?: string
+}
+
+/**
+ *
+ */
+export default function powerCrypt(str: string, options: PowerCryptOptions = {}): string {
+  let { nStretch, algorithm, encode, salt } = options
+  salt = salt || ''
+  encode = encode || 'base64'
+  algorithm = algorithm || 'sha256'
+  nStretch = Math.max(nStretch || 1000, 1)
+
+  let i = 0
+  let buf = str + salt
+  const hashFn = shajs(algorithm)
+  while (i < nStretch) {
+    buf = hashFn.update(buf).digest()
+    i++
+  }
+  return buf.toString(encode)
+}
