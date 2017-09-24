b1c5928c64306cc0975e9b4983a4853ea14617d6
Shin Suzuki <shinout310@gmail.com>
2017-09-24 23:34:14 +0900

    [power-filter] [WIP] checking condition

--- modules/power-filter/src/index.js
+++ modules/power-filter/src/index.js
@@ -2,15 +2,66 @@
 
 import type {
   RestorableEntity,
+  DotNotationString,
   WhereConditions,
+  QueryCondition,
 } from 'phenyl-interfaces'
+
+import {
+  normalizeQueryCondition,
+} from 'phenyl-utils/jsnext'
 
 /**
  *
  */
 export default class PowerFilter {
   static find(values: Array<RestorableEntity>, where: WhereConditions): Array<RestorableEntity> {
-    return values
+    const dotNotations = Object.keys(where)
+    return values.filter(value =>
+      dotNotations.every(dotNotation =>
+
+        this.checkCondition(value, dotNotation, where[dotNotation])
+      )
+    )
+  }
+
+  static checkCondition(value: RestorableEntity, dotNotation: DotNotationString, condition: QueryCondition): boolean {
+    const operators = Object.keys(condition)
+    return operators.every(operator => {
+      switch (operator) {
+        case '$eq':
+        case '$gt':
+        case '$gte':
+        case '$in':
+        case '$lt':
+        case '$lte':
+        case '$ne':
+        case '$nin':
+        case '$and':
+        case '$not':
+        case '$nor':
+        case '$or':
+        case '$exists':
+        case '$type':
+        case '$mod':
+        case '$regex':
+        case '$text':
+        case '$where':
+        case '$geoIntersects':
+        case '$geoWithin':
+        case '$near':
+        case '$nearSphere':
+        case '$all':
+        case '$elemMatch':
+        case '$size':
+        case '$bitsAllClear':
+        case '$bitsAllSet':
+        case '$bitsAnyClear':
+        case '$bitsAnySet':
+        default:
+          return true // TODO
+      }
+    })
   }
 
   static findOne(values: Array<RestorableEntity>, where: WhereConditions): RestorableEntity {
