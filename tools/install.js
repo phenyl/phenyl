c73129820c0436900d1b99be6d4c88f771bd82af
Shin Suzuki <shinout310@gmail.com>
2017-09-29 20:06:47 +0900

    [tools] improve installation command

--- tools/install.js
+++ tools/install.js
@@ -1,41 +1,76 @@
 const shell = require('shelljs')
 const path = require('path')
 
-const phenylModules = shell.exec(
-  'ls -F ./modules | grep / | tr -d "/"',
-  { silent: true }
-).trim().split('\n')
+class ModuleLoader {
+  constructor() {
+    this.visitedModules = {}
+    this.loadedModules = {}
 
-const linkPhenylModules = (moduleNames) => {
-  moduleNames.forEach(module => {
-    shell.cd('node_modules')
-    shell.echo(`link ${module}`)
-    shell.ln('-s', `../../${module}`, module)
-    shell.cd('../')
-  })
+    shell.cd(__dirname + '/..')
+    this.modules = shell.exec(
+      'ls -F ./modules | grep / | tr -d "/"',
+      { silent: true }
+    ).trim().split('\n')
+  }
+
+  load() {
+    this.modules.forEach(moduleName => {
+      this.installSubModules(moduleName)
+    })
+  }
+
+  installSubModules(moduleName) {
+    if (this.loadedModules[moduleName]) {
+      return
+    }
+
+    if (this.visitedModules[moduleName]) {
+      throw new Error('Circular dependencies detected.')
+    }
+
+    this.visitedModules[moduleName] = true
+
+    const dependentModules = this.getDependentModules(moduleName)
+
+    if (dependentModules.length) {
+      dependentModules.forEach(depModuleName => {
+        this.linkModule(depModuleName, moduleName)
+        this.installSubModules(depModuleName)
+      })
+    }
+
+    console.log(`[${moduleName}] All dependent modules were resolved.`)
+    console.log(`[${moduleName}] Running npm install.`)
+    shell.cd(`modules/${moduleName}/`)
+    shell.exec('npm install')
+    shell.cd('../../')
+    console.log(`[${moduleName}] install done.\n`)
+    this.loadedModules[moduleName] = true
+  }
+
+  linkModule(moduleName, ownerModuleName) {
+    shell.mkdir('-p', `modules/${ownerModuleName}/node_modules`)
+    shell.cd(`modules/${ownerModuleName}/node_modules`)
+    console.log(`[${ownerModuleName}] linking ${moduleName}`)
+    shell.ln('-s', `../../${moduleName}`, moduleName)
+    shell.cd('../../../')
+  }
+
+  getDependentModules(moduleName) {
+    const packageJson = require(path.join(__dirname, `../modules/${moduleName}/package.json`))
+    let dependentModules = []
+
+    if (packageJson.dependencies != null) {
+      const dependencies = Object.keys(packageJson.dependencies)
+      dependentModules = dependentModules.concat(dependencies.filter(dependency => (this.modules.includes(dependency))))
+    }
+
+    if (packageJson.devDependencies != null) {
+      const devDependencies = Object.keys(packageJson.devDependencies)
+      dependentModules = dependentModules.concat(devDependencies.filter(devDependency => (this.modules.includes(devDependency))))
+    }
+    return dependentModules
+  }
 }
 
-shell.exec('npm i')
-
-phenylModules.forEach(module => {
-  shell.cd(`modules/${module}`)
-
-  const packageJson = require(path.join(__dirname, `../modules/${module}/package.json`))
-  let modulesInPhenyl = []
-
-  if (packageJson.dependencies != null) {
-    const dependencies = Object.keys(packageJson.dependencies)
-    modulesInPhenyl = modulesInPhenyl.concat(dependencies.filter(dependency => (phenylModules.includes(dependency))))
-  }
-
-  if (packageJson.devDependencies != null) {
-    const devDependencies = Object.keys(packageJson.devDependencies)
-    modulesInPhenyl = modulesInPhenyl.concat(devDependencies.filter(devDependency => (phenylModules.includes(devDependency))))
-  }
-
-  shell.mkdir('-p', 'node_modules')
-  linkPhenylModules(modulesInPhenyl)
-  shell.exec('npm i')
-
-  shell.cd('../../')
-})
+new ModuleLoader().load()
