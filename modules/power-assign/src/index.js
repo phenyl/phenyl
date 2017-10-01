10392d898c345dd694f1ba3e4f4992707e63192a
Shin Suzuki <shinout310@gmail.com>
2017-10-01 16:48:07 +0900

    [power-assign] export functions

--- modules/power-assign/src/index.js
+++ modules/power-assign/src/index.js
@@ -6,7 +6,11 @@ export {
   assignWithRestoration,
   assignToPropWithRestoration,
 } from './power-assign.js'
-export { retargetToProp } from './retarget-to-prop.js'
+export {
+  retargetToProp,
+  retargetToPropWithRestoration,
+} from './retarget-to-prop.js'
+export { toJSON } from './to-json.js'
 export { unassignProp } from './unassign-prop.js'
 export { getObjectsToBeAssigned } from './get-objects-to-be-assigned.js'
 export default PowerAssign
