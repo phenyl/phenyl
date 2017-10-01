f133b3538785a976570b926a2fbe24cb2a25471e
Shin Suzuki <shinout310@gmail.com>
2017-10-02 08:16:14 +0900

    [phenyl-utils] in getNestedValue(), throw an error when non-existing document path is given

--- modules/phenyl-utils/src/get-nested-value.js
+++ modules/phenyl-utils/src/get-nested-value.js
@@ -8,5 +8,12 @@ import type {
 } from 'phenyl-interfaces'
 
 export function getNestedValue(obj: Restorable, docPath: DocumentPath): any {
-  return parseDocumentPath(docPath).reduce((currentObj, key) => currentObj[key], obj)
+  return parseDocumentPath(docPath).reduce((currentObj, key) => {
+    try {
+      return currentObj[key]
+    }
+    catch (e) {
+      throw new Error(`Cannot get value by the document path: "${docPath}". The property "${key}" is not found in the upper undefined object.`)
+    }
+  }, obj)
 }
