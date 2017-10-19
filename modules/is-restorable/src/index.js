6db6ab60db10f87386d895cfdbbaa914dde75bad
Shin Suzuki <shinout310@gmail.com>
2017-10-20 01:22:35 +0900

    [is-restorable] first commit

--- modules/is-restorable/src/index.js
+++ modules/is-restorable/src/index.js
@@ -1,5 +1,13 @@
 // @flow
+import deepEqual from 'fast-deep-equal'
+
 export default function isRestorable(obj: any): boolean {
-  // TODO
-  return true
+  if (obj == null || typeof obj != 'object' || obj.constructor === Object) {
+    return true
+  }
+
+  const plain = JSON.parse(JSON.stringify(obj))
+
+  const newObj = new obj.constructor(plain)
+  return deepEqual(obj, newObj)
 }
