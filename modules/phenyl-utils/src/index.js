007ecd6c879c5316581868ab318e3ef0fe25db95
Shin Suzuki <shinout310@gmail.com>
2017-10-22 22:46:56 +0900

    [phenyl-utils] remove duplicated functions with oad-utils

--- modules/phenyl-utils/src/index.js
+++ modules/phenyl-utils/src/index.js
@@ -2,10 +2,3 @@
 export * from './request-assertion.js'
 export { createErrorResult } from './create-error-result.js'
 export { normalizeQueryCondition } from './normalize-query-condition.js'
-export { getNestedValue } from './get-nested-value.js'
-export { sortByNotation } from './sort-by-notation.js'
-export {
-  parseDocumentPath,
-  convertToDotNotationString,
-  createDocumentPath,
-} from './document-path.js'
