37c0499f26c0014c53f60e8a69017caf6f5edb7e
Shin Suzuki <shinout310@gmail.com>
2017-10-26 02:33:25 +0900

    [phenyl-standards] add createCustomPathModifiers

--- modules/phenyl-standards/src/index.js
+++ modules/phenyl-standards/src/index.js
@@ -3,10 +3,12 @@ import StandardEntityDefinition from './standard-entity-definition.js'
 import StandardUserDefinition from './standard-user-definition.js'
 import { encryptPasswordInRequestData } from './encrypt-password-in-request-data.js'
 import { removePasswordFromResponseData } from './remove-password-from-response-data.js'
+import createCustomPathModifiers from './create-custom-path-modifiers.js'
 
 export {
   StandardEntityDefinition,
   StandardUserDefinition,
   removePasswordFromResponseData,
   encryptPasswordInRequestData,
+  createCustomPathModifiers,
 }
