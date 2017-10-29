feee4c90f9519499ee11d77f6a7aa7896fb84e8c
imoans <naomins0101@gmail.com>
2017-10-29 17:45:54 +0900

    :wrench: create shell script to test in all modules

---
+++ tools/lib/phenyl-module.js
@@ -0,0 +1,27 @@
+const shell = require('shelljs')
+module.exports = class PhenylModule {
+  constructor(moduleName) {
+    this.moduleName = moduleName
+    this.passingTest = false
+  }
+
+  test() {
+    const pathToModuleFromRoot = `modules/${this.moduleName}`
+    shell.cd(pathToModuleFromRoot)
+    const scripts = require('../../' + pathToModuleFromRoot + '/package.json').scripts
+
+    if (scripts && Object.keys(scripts).includes('test')) {
+      if (shell.exec('npm test --color always').code === 0) {
+        this.passingTest = true
+      }
+    } else {
+      shell.echo(`no test specified in ${this.moduleName}`)
+    }
+
+    shell.cd(`../../`)
+  }
+
+  build() {
+    shell.exec(`BABEL_ENV=build babel modules/${this.moduleName}/src -d modules/${this.moduleName}/dist`)
+  }
+}
