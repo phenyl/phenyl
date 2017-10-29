bed5ac424c9f791fc7c01ef7fa5b657c2c246a31
imoans <naomins0101@gmail.com>
2017-10-29 22:08:10 +0900

    :wrench: update load script

---
+++ tools/load.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const PhenylModule = require('./lib/phenyl-module')
+const ModuleLoader = require('./lib/module-loader')
+
+const pathToModules = __dirname + '/../modules'
+const moduleNames = fs.readdirSync(pathToModules).filter(moduleName => fs.statSync(pathToModules + '/' + moduleName).isDirectory())
+
+const phenylModules = moduleNames.map(moduleName => new PhenylModule(moduleName))
+const moduleLoader = new ModuleLoader(moduleNames)
+
+phenylModules.forEach(phenylModule => moduleLoader.load(phenylModule.moduleName))
