0d88ee899aab4a2a10d901f6382005a7a2307a5d
imoans <naomins0101@gmail.com>
2017-11-03 17:28:50 +0900

    :hammer: return error result if an error occurs

--- modules/phenyl-mongodb-client/src/index.js
+++ modules/phenyl-mongodb-client/src/index.js
@@ -1,6 +1,8 @@
 // @flow
 import promisify from 'es6-promisify'
 
+import { createErrorResult } from 'phenyl-utils/jsnext.js'
+
 import type {
   EntityClient,
   IdQuery,
@@ -51,87 +53,112 @@ export default class PhenylMongoDbClient implements EntityClient {
     if (skip) options.skip = skip
     if (limit) options.limit = limit
 
-    const result = await coll.find(set_idToId(where), options)
-    return {
-      ok: 1,
-      values: result
+    try {
+      const result = await coll.find(set_idToId(where), options)
+      return { ok: 1, values: result }
+    }
+    catch (error) {
+      return createErrorResult(error)
     }
   }
 
   async findOne(query: WhereQuery): Promise<SingleQueryResultOrError> {
     const { entityName, where } = query
     const coll = this.conn.collection(entityName)
-    const result = await coll.find(set_idToId(where), { limit: 1 })
-    return {
-      ok: 1,
-      value: result[0]
+    try {
+      const result = await coll.find(set_idToId(where), { limit: 1 })
+      return { ok: 1, value: result[0] || null }
+    } catch (error) {
+      return createErrorResult(error)
     }
   }
 
   async get(query: IdQuery): Promise<SingleQueryResultOrError> {
     const { entityName, id } = query
     const coll = this.conn.collection(entityName)
-    const result = await coll.find({ _id: id })
-    return {
-      ok: 1,
-      value: result[0]
+    try {
+      const result = await coll.find({ _id: id })
+      return { ok: 1, value: result[0] || null }
+    } catch (error) {
+      return createErrorResult(error)
     }
   }
 
   async getByIds(query: IdsQuery): Promise<QueryResultOrError> {
     const { entityName, ids } = query
     const coll = this.conn.collection(entityName)
-    const result = await coll.find({ _id: { $in: ids } })
-    return {
-      ok: 1,
-      values: result
+    try {
+      const result = await coll.find({ _id: { $in: ids } })
+      return { ok: 1, values: result }
+    } catch (error) {
+      return createErrorResult(error)
     }
   }
 
   async insert(command: InsertCommand): Promise<CommandResultOrError> {
     const { entityName } = command
     const coll = this.conn.collection(entityName)
-    if (command.value) {
-      const result = await coll.insertOne(command.value)
-      return { ok: 1, n: 1 }
+    let result
+    try {
+      if (command.value) {
+        result = await coll.insertOne(command.value)
+      }
+      else {
+        result = await coll.insertMany(command.values)
+      }
+      return { ok: 1, n: result.insertedCount }
+    } catch (error) {
+      return createErrorResult(error)
     }
-    const result = await coll.insertMany(command.values)
-    return { ok: 1, n: result.insertedCount }
   }
 
   async insertAndGet(command: SingleInsertCommand): Promise<GetCommandResultOrError> {
     const { entityName } = command
     const coll = this.conn.collection(entityName)
-    const result = await coll.insertOne(command.value)
-    const insertedValue = await this.get({
-      entityName,
-      id: result.insertedId,
-    })
-    return { ok: 1, n: 1, value: insertedValue.value }
+    try {
+      const result = await coll.insertOne(command.value)
+      const insertedResult = await this.get({
+        entityName,
+        id: result.insertedId,
+      })
+      return { ok: 1, n: result.insertedCount, value: insertedResult.value }
+    } catch (error) {
+      return createErrorResult(error)
+    }
   }
 
   async insertAndGetMulti(command: MultiInsertCommand): Promise<MultiValuesCommandResultOrError> {
     const { entityName } = command
     const coll = this.conn.collection(entityName)
-    const result = await coll.insertMany(command.values)
-    const insertedValues = await this.getByIds({
-      entityName,
-      ids: result.insertedIds,
-    })
-    return { ok: 1, n: 1, values: insertedValues.values }
+
+    try {
+      const result = await coll.insertMany(command.values)
+      const insertedResult = await this.getByIds({
+        entityName,
+        ids: result.insertedIds,
+      })
+      return { ok: 1, n: result.insertedCount, values: insertedResult.values }
+    } catch (error) {
+      return createErrorResult(error)
+    }
   }
 
   async update(command: UpdateCommand): Promise<CommandResultOrError> {
     const { entityName, operation } = command
     const coll = this.conn.collection(entityName)
+    let result
 
-    if (command.id) {
-      const result = await coll.updateOne({ _id: command.id }, operation)
-      return { ok: 1, ng: 1 }
-    }
-    if (command.where) {
-      const result = await coll.updateMany(command.where, operation)
-      return { ok: 1, ng: 1 }
+    try {
+      if (command.id) {
+        result = await coll.updateOne({ _id: command.id }, operation)
+      }
+      if (command.where) {
+        result = await coll.updateMany(command.where, operation)
+      }
+      const { nModified, nMatched } = result
+      return { ok: 1, nMatched, nModified }
+    } catch (error) {
+      return createErrorResult(error)
     }
     throw new Error(`Invalid Update command is given to PhenylMongoDbClient#update().`)
   }
@@ -139,35 +166,48 @@ export default class PhenylMongoDbClient implements EntityClient {
   async updateAndGet(command: IdUpdateCommand): Promise<GetCommandResultOrError> {
     const { entityName, id, operation } = command
     const coll = this.conn.collection(entityName)
-    const result = await coll.updateOne({ _id: id }, operation)
-    const { modifiedCount } = result
-    if (modifiedCount > 0) {
-      const updatedResult = await this.get({ entityName, id })
-      return { ok: 1, n: modifiedCount, value: updatedResult.value }
-    }
-    else {
-      return { ok: 1, n: 0, value: null }
+    try {
+      const result = await coll.updateOne({ _id: id }, operation)
+      const { modifiedCount } = result
+      if (modifiedCount > 0) {
+        const updatedResult = await this.get({ entityName, id })
+        return { ok: 1, n: modifiedCount, value: updatedResult.value }
+      }
+      else {
+        return { ok: 1, n: 0, value: null }
+      }
+    } catch (error) {
+      return createErrorResult(error)
     }
   }
 
   async updateAndFetch(command: MultiUpdateCommand): Promise<MultiValuesCommandResultOrError> {
     const { entityName, where, operation } = command
     const coll = this.conn.collection(entityName)
-    const result = await coll.updateMany(where, operation)
-    const updatedResult = await this.find({ entityName, where })
-    return { ok: 1, n: 1, values: updatedResult.values }
+    try {
+      const result = await coll.updateMany(where, operation)
+      const updatedResult = await this.find({ entityName, where })
+      const { nModified, nMatched } = result
+      return { ok: 1, nModified, nMatched, values: updatedResult.values }
+    } catch (error) {
+      createErrorResult(error)
+    }
   }
 
   async delete(command: DeleteCommand): Promise<CommandResultOrError> {
     const { entityName } = command
     const coll = this.conn.collection(entityName)
-    if (command.id) {
-      const result = await coll.deleteOne({ _id: command.id })
-      return { ok: 1, n: 1 }
-    }
-    if (command.where) {
-      const result = await coll.deleteMany(command.where)
+    let result
+    try {
+      if (command.id) {
+        result = await coll.deleteOne({ _id: command.id })
+      }
+      if (command.where) {
+        result = await coll.deleteMany(command.where)
+      }
       return { ok: 1, n: result.deletedCount }
+    } catch (error) {
+      createErrorResult(error)
     }
     throw new Error(`Invalid Delete command is given to PhenylMongoDbClient#delete().`)
   }
