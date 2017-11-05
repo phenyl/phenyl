f7a628b40ba88d947d352758f67a8dbacd4dff05
Shin Suzuki <shinout310@gmail.com>
2017-11-05 21:32:52 +0900

    [phenyl-utils] fix bug: PhenylCoreClient returns payload when succeeded

--- modules/phenyl-utils/src/phenyl-core-client.js
+++ modules/phenyl-utils/src/phenyl-core-client.js
@@ -62,8 +62,8 @@ export class PhenylCoreClient implements CoreClient {
   async find(query: WhereQuery, sessionId?: ?Id): Promise<QueryResult> {
     const reqData = { method: 'find', payload: query, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'find' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "find" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'find') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -72,8 +72,8 @@ export class PhenylCoreClient implements CoreClient {
   async findOne(query: WhereQuery, sessionId?: ?Id): Promise<SingleQueryResult> {
     const reqData = { method: 'findOne', payload: query, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'findOne' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "findOne" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'findOne') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -82,8 +82,8 @@ export class PhenylCoreClient implements CoreClient {
   async get(query: IdQuery, sessionId?: ?Id): Promise<SingleQueryResult> {
     const reqData = { method: 'get', payload: query, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'get' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "get" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'get') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -92,8 +92,8 @@ export class PhenylCoreClient implements CoreClient {
   async getByIds(query: IdsQuery, sessionId?: ?Id): Promise<QueryResult> {
     const reqData = { method: 'getByIds', payload: query, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'getByIds' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "getByIds" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'getByIds') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -102,8 +102,8 @@ export class PhenylCoreClient implements CoreClient {
   async insert(command: InsertCommand, sessionId?: ?Id): Promise<CommandResult> {
     const reqData = { method: 'insert', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'insert' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "insert" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'insert') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -112,8 +112,8 @@ export class PhenylCoreClient implements CoreClient {
   async insertAndGet(command: SingleInsertCommand, sessionId?: ?Id): Promise<GetCommandResult> {
     const reqData = { method: 'insertAndGet', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'insertAndGet' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "insertAndGet" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'insertAndGet') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -122,8 +122,8 @@ export class PhenylCoreClient implements CoreClient {
   async insertAndGetMulti(command: MultiInsertCommand, sessionId?: ?Id): Promise<MultiValuesCommandResult> {
     const reqData = { method: 'insertAndGetMulti', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'insertAndGetMulti' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "insertAndGetMulti" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'insertAndGetMulti') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -132,8 +132,8 @@ export class PhenylCoreClient implements CoreClient {
   async update(command: UpdateCommand, sessionId?: ?Id): Promise<CommandResult> {
     const reqData = { method: 'update', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'update' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "update" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'update') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -142,8 +142,8 @@ export class PhenylCoreClient implements CoreClient {
   async updateAndGet(command: IdUpdateCommand, sessionId?: ?Id): Promise<GetCommandResult> {
     const reqData = { method: 'updateAndGet', payload: command, sessionId}
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'updateAndGet' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "updateAndGet" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'updateAndGet') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -152,8 +152,8 @@ export class PhenylCoreClient implements CoreClient {
   async updateAndFetch(command: MultiUpdateCommand, sessionId?: ?Id): Promise<MultiValuesCommandResult> {
     const reqData = { method: 'updateAndFetch', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'updateAndFetch' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "updateAndFetch" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'updateAndFetch') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -162,8 +162,8 @@ export class PhenylCoreClient implements CoreClient {
   async delete(command: DeleteCommand, sessionId?: ?Id): Promise<CommandResult> {
     const reqData = { method: 'delete', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'delete' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "delete" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'delete') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -172,8 +172,8 @@ export class PhenylCoreClient implements CoreClient {
   async runCustomQuery(query: CustomQuery, sessionId?: ?Id): Promise<CustomQueryResult> {
     const reqData = { method: 'runCustomQuery', payload: query, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'runCustomQuery' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "runCustomQuery" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'runCustomQuery') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -182,8 +182,8 @@ export class PhenylCoreClient implements CoreClient {
   async runCustomCommand(command: CustomCommand, sessionId?: ?Id): Promise<CustomCommandResult> {
     const reqData = { method: 'runCustomCommand', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'runCustomCommand' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "runCustomCommand" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'runCustomCommand') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -192,8 +192,8 @@ export class PhenylCoreClient implements CoreClient {
   async login(command: LoginCommand, sessionId?: ?Id): Promise<LoginCommandResult> {
     const reqData = { method: 'login', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'login' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "login" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'login') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 
   /**
@@ -202,7 +202,7 @@ export class PhenylCoreClient implements CoreClient {
   async logout(command: LogoutCommand, sessionId?: ?Id): Promise<LogoutCommandResult> {
     const reqData = { method: 'logout', payload: command, sessionId }
     const resData = await this.handleRequestData(reqData)
-    if (resData.type === 'logout' || resData.type === 'error') throw createErrorResult(resData.payload)
-    throw new Error(`Invalid response data: type must be "logout" or "error". "${resData.type}" is given.`)
+    if (resData.type === 'logout') return resData.payload
+    throw createErrorResult(resData.type === 'error' ? resData.payload : `Unexpected response type "${resData.type}".`)
   }
 }
