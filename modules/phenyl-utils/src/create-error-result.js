c7db082aca58c63200e212c2999a4b9c6b536f03
Shin Suzuki <shinout310@gmail.com>
2017-11-05 15:08:37 +0900

    [phenyl-utils] createErrorResult returns instance of Error

--- modules/phenyl-utils/src/create-error-result.js
+++ modules/phenyl-utils/src/create-error-result.js
@@ -4,13 +4,30 @@ import type {
   ErrorResultType,
 } from 'phenyl-interfaces'
 
-export function createErrorResult(error: Error, type?: ErrorResultType): ErrorResult {
-  return {
-    ok: 0,
-    type: type || guessErrorType(error),
-    message: error.message,
-    stack: error.stack
+export class PhenylResponseError extends Error implements ErrorResult {
+  type: ErrorResultType
+  ok: 0
+
+  constructor(message: string, type?: ErrorResultType) {
+    super(message)
+    this.type = type || 'InternalServer'
+    this.ok = 0
   }
+
+  toJSON(): ErrorResult {
+    return {
+      ok: 0,
+      type: this.type,
+      message: this.message,
+      stack: this.stack
+    }
+  }
+}
+
+export function createErrorResult(error: Error, type?: ErrorResultType): ErrorResult {
+  const responseError = new PhenylResponseError(error.message, type)
+  responseError.stack = error.stack
+  return responseError
 }
 
 function guessErrorType(error: Error): ErrorResultType {
--- modules/phenyl-utils/src/index.js
+++ modules/phenyl-utils/src/index.js
@@ -1,6 +1,6 @@
 // @flow
 export * from './request-assertion.js'
-export { createErrorResult } from './create-error-result.js'
+export { createErrorResult, PhenylResponseError } from './create-error-result.js'
 export { normalizeQueryCondition } from './normalize-query-condition.js'
 export { PhenylCoreClient } from './phenyl-core-client.js'
 export { switchByRequestMethod } from './switch-by-request-method.js'
