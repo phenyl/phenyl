e6ce30c6636ed0b583c2ac9e677491db9109d886
Shin Suzuki <shinout310@gmail.com>
2017-11-13 00:09:05 +0900

    [phenyl-standards] Add removePasswordFromResponseData()

--- modules/phenyl-standards/src/remove-password-from-response-data.js
+++ modules/phenyl-standards/src/remove-password-from-response-data.js
@@ -1,9 +1,23 @@
 // @flow
+import {
+  hasOwnNestedProperty,
+} from 'oad-utils/jsnext'
+
+import {
+  visitEntitiesInResponseData,
+} from 'phenyl-utils/jsnext'
+
+import {
+  assign,
+} from 'power-assign/jsnext'
+
 import type {
   ResponseData,
 } from 'phenyl-interfaces'
 
-export function removePasswordFromResponseData(resData: ResponseData, passwordPropName: string): ResponseData { // eslint-disable-line no-unused-vars
-  // TODO
-  return resData
+export function removePasswordFromResponseData(resData: ResponseData, passwordPropName: string): ResponseData {
+  return visitEntitiesInResponseData(resData, entity => {
+    if (!hasOwnNestedProperty(entity, passwordPropName)) return entity
+    return assign(entity, { $unset: { [passwordPropName]: '' } })
+  })
 }
