30d082a2a14ce60d0d59fab05a13c2f2b339c90c
Shin Suzuki <shinout310@gmail.com>
2017-11-13 00:08:11 +0900

    [phenyl-utils] Add visitEntitiesInResponseData()

---
+++ modules/phenyl-utils/src/visit-entities-in-response-data.js
@@ -0,0 +1,57 @@
+// @flow
+import {
+  assign
+} from 'power-assign/jsnext'
+
+import type {
+  Entity,
+  ResponseData,
+} from 'phenyl-interfaces'
+
+
+export type EntityVisitor = (entity: Entity) => Entity
+
+/**
+ *
+ */
+export function visitEntitiesInResponseData(resData: ResponseData, visitor: EntityVisitor): ResponseData {
+  switch (resData.type) {
+    case 'get':
+    case 'findOne':
+    case 'insertAndGet':
+    case 'updateAndGet': {
+      const newEntity = visitor(resData.payload.entity)
+      if (newEntity === resData.payload.entity) return resData
+      return assign(resData, { 'payload.entity': newEntity })
+    }
+    case 'getByIds':
+    case 'find':
+    case 'insertAndGetMulti':
+    case 'updateAndFetch': {
+      const newEntities = resData.payload.entities.map(visitor)
+      return assign(resData, { 'payload.entities': newEntities })
+    }
+    case 'pull':
+    case 'push': {
+      if (resData.payload.entity == null) return resData
+      const newEntity = visitor(resData.payload.entity)
+      if (newEntity === resData.payload.entity) return resData
+      return assign(resData, { 'payload.entity': newEntity })
+    }
+    case 'login': {
+      if (resData.payload.user == null) return resData
+      const newUser = visitor(resData.payload.user)
+      if (newUser === resData.payload.user) return resData
+      return assign(resData, { 'payload.user': newUser })
+    }
+
+    case 'insert':
+    case 'update':
+    case 'delete':
+    case 'logout': {
+      return resData
+    }
+    default:
+      return resData
+  }
+}
