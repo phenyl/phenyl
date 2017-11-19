5f8824a4f51fb8f2cdd9761308c5faa2a33fd43f
imoans <naomins0101@gmail.com>
2017-11-19 21:12:22 +0900

    :hammer: Use StringMapBuilder and RestorableBuilder

--- modules/power-string-storage/src/index.js
+++ modules/power-string-storage/src/index.js
@@ -1,8 +1,8 @@
 // @flow
 import StringMapBuilder from './string-map-builder.js'
 import type { Restorable } from 'phenyl-interfaces'
-import type { StringStorage } from '../decls/async-storage.js.flow'
-import type { StringMap } from '../decls/string-map.js.flow'
+import type { StringStorage } from '../decls/string-storage.js.flow'
+import RestorableBuilder from './restorable-builder.js'
 
 export default class PowerStringStorage {
   storage: StringStorage
@@ -17,19 +17,48 @@ export default class PowerStringStorage {
       nameSpace,
       maxDepth,
     })
-
     const values = builder.build()
+    const valuesToSet = []
+    Object.keys(values).map(key => {
+      valuesToSet.push([key, values[key]])
+    })
+    try {
+      await this.storage.multiSet(valuesToSet)
+    } catch (error) {
+      return false
+    }
+    return true
   }
 
   async clear(): Promise<boolean> {
     const allKeys = await this.storage.getAllKeys()
-    return this.storage.multiRemove(allKeys)
+    try {
+      this.storage.multiRemove(allKeys)
+    } catch (error) {
+      return false
+    }
+    return true
   }
 
-  restore(): Promise<Restorable> {
+  async restore(nameSpace: string): Promise<Restorable> {
+    const metaInfoStr = await this.storage.getItem(nameSpace)
+    const metaInfo = JSON.parse(metaInfoStr)
+    const { ids } = metaInfo
+    const keyValueArray = await this.storage.multiGet(ids)
+    const valueByKeys = keyValueArray.reduce((total, item) =>
+        Object.assign(total, { [item[0]]: item[1] })
+    , {})
+
+    const restorableBuilder = new RestorableBuilder({
+      metaInfo,
+      valuesByKeys,
+    })
+
+    const restorable = restorableBuilder.build()
+    return restorable
   }
 
-  assign(ops: SetUnsetOperator): Promise<boolean> {
+  assign(ops): Promise<boolean> {
 
   }
 }
--- modules/power-string-storage/test/index.js
+++ modules/power-string-storage/test/index.js
@@ -1,7 +1,49 @@
 // @flow
 
-import type { Restorable } from 'phenyl-interfaces'
-import { describe, it, before } from 'kocha'
+import { describe, it } from 'kocha'
 import PowerStringStorage from '../src'
 import assert from 'power-assert'
 
+describe('', function () {
+  it('', function () {
+    const storage = {
+      'user': '{"maxDepth":3,"ids":["user.name.first","user.name.last","user.favorites.music.1","user.favorites.music.2","user.favorites.music","user.favorites.movie.1","user.favorites.movie"]}',
+      'user.name.first': '"Naomi"',
+      'user.name.last': '"Campbell"',
+      'user.favorites.music.1': '{"title":"LA・LA・LA LOVE SONG","singer":"Toshinobu Kubota"}',
+      'user.favorites.music.2': '{"title":"Windy Lady","singer":"Tatsuro Yamashita"}',
+      'user.favorites.music': '{"ids":[1,2],"lastInsertId":2}',
+      'user.favorites.movie.1': '{"title":"Ugly Betty","year":2007}',
+      'user.favorites.movie': '{"ids":[1],"lastInsertId":1}',
+    }
+
+    const powerStringStorage = new PowerStringStorage()
+    const user = powerStringStorage.restore('user')
+
+    const expectedUser = {
+      name: {
+        first: 'Naomi',
+        last: 'Campbell',
+      },
+      favorites: {
+        music: [
+          {
+            title: 'LA・LA・LA LOVE SONG',
+            singer: 'Toshinobu Kubota',
+          },
+          {
+            title: 'Windy Lady',
+            singer: 'Tatsuro Yamashita',
+          }
+        ],
+        movie: [
+          {
+            title: 'Ugly Betty',
+            year: 2007,
+          }
+        ]
+      },
+    }
+    assert.deepEqual(expectedUser, user)
+  })
+})
