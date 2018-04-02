eb5fdc0d24ed56745d6427fe15d60a15f73cff1f
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-04-02 15:51:30 +0900

    Install specific mongo packages in CircleCI

--- tools/scripts/install-mongodb-on-ubuntu.sh
+++ tools/scripts/install-mongodb-on-ubuntu.sh
@@ -4,11 +4,11 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
 # See https://serverfault.com/questions/746348/mongodb-install-fails-in-ubuntu-14-04-docker-container
-echo "deb [ arch=amd64 ] http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
 # See https://askubuntu.com/questions/104160/method-driver-usr-lib-apt-methods-https-could-not-be-found-update-error
 sudo apt-get install apt-transport-https
 
 sudo apt-get update
 
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org=3.6.3 mongodb-org-server=3.6.3 mongodb-org-shell=3.6.3 mongodb-org-mongos=3.6.3 mongodb-org-tools=3.6.3
