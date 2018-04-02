a759a21d9057f06fd5a59a3c81d5086c84282e8b
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-04-02 13:18:22 +0900

    Install mongodb for Ubuntu14.04 in CircleCi

--- tools/scripts/install-mongodb-on-ubuntu.sh
+++ tools/scripts/install-mongodb-on-ubuntu.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+# See https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+# See https://serverfault.com/questions/746348/mongodb-install-fails-in-ubuntu-14-04-docker-container
+echo "deb [ arch=amd64 ] http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
 # See https://askubuntu.com/questions/104160/method-driver-usr-lib-apt-methods-https-could-not-be-found-update-error
 sudo apt-get install apt-transport-https
