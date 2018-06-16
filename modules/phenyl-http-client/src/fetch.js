ca1796b42f24172fb9961c95150f530b827422d8
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-06-16 13:19:38 +0900

    [phenyl-http-client] Tricky workaround for use in React Native

--- modules/phenyl-http-client/src/fetch.js
+++ modules/phenyl-http-client/src/fetch.js
@@ -1,2 +1,14 @@
 // @flow
-export { default } from 'node-fetch'
+
+/**
+ * This is the tricky workaround for Metro bundler (used in React Native).
+ * As Metro bundler doesn't see "browser" field in package.json, which otherwise switch the file from "fetch.js"
+ * to "fetch-browser.js" when "fetch.js" is required. Then, Metro bundler parses this file and try to bundle
+ * "node-fetch", which is intended to run only on Node.js. Metro bundler is wise enough to see the strings passed to
+ * require function. For Metro bundler not to follow the module name, generating the string programatically like
+ * `join('-')` is needed.
+ */
+const moduleName = ['node', 'fetch'].join('-')
+
+// $FlowIssue(fetch-maybe-globally-assigned)
+export default typeof fetch === 'undefined' ? require(moduleName) : fetch
