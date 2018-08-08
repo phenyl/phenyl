3d9d8c307c623b7f119f757bdc13333dc3bf3744
Leko <leko.noor@gmail.com>
2018-08-08 14:40:16 +0900

    Make components to SFC

--- modules/phenyl-api-explorer/src/client/src/App.js
+++ modules/phenyl-api-explorer/src/client/src/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { Route, Switch } from 'react-router-dom'
@@ -15,105 +15,77 @@ import OperationEditor from './containers/OperationEditor'
 import OperationResult from './containers/OperationResult'
 import './App.css'
 
-class Home extends Component {
-  render () {
-    return (
-      null
-    )
-  }
-}
-class Users extends Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumb />
-        <Divider horizontal>Settings</Divider>
-        <OperationEditor />
-        <Divider horizontal>Result</Divider>
-        <OperationResult />
-      </div>
-    )
-  }
-}
-class NonUsers extends Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumb />
-        <Divider horizontal>Settings</Divider>
-        <OperationEditor />
-        <Divider horizontal>Result</Divider>
-        <OperationResult />
-      </div>
-    )
-  }
-}
-class CustomQuery extends Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumb />
-        <Divider horizontal>Settings</Divider>
-        <OperationEditor />
-        <Divider horizontal>Result</Divider>
-        <OperationResult />
-      </div>
-    )
-  }
-}
-class CustomCommand extends Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumb />
-        <Divider horizontal>Settings</Divider>
-        <OperationEditor />
-        <Divider horizontal>Result</Divider>
-        <OperationResult />
-      </div>
-    )
-  }
-}
-class NotFound extends Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumb />
-        <Divider horizontal>Settings</Divider>
-        <OperationEditor />
-        <Divider horizontal>Result</Divider>
-        <OperationResult />
-      </div>
-    )
-  }
-}
+const Home = () => (
+  null
+)
+const Users = () => (
+  <div>
+    <Breadcrumb />
+    <Divider horizontal>Settings</Divider>
+    <OperationEditor />
+    <Divider horizontal>Result</Divider>
+    <OperationResult />
+  </div>
+)
+const NonUsers = () => (
+  <div>
+    <Breadcrumb />
+    <Divider horizontal>Settings</Divider>
+    <OperationEditor />
+    <Divider horizontal>Result</Divider>
+    <OperationResult />
+  </div>
+)
+const CustomQuery = () => (
+  <div>
+    <Breadcrumb />
+    <Divider horizontal>Settings</Divider>
+    <OperationEditor />
+    <Divider horizontal>Result</Divider>
+    <OperationResult />
+  </div>
+)
+const CustomCommand = () => (
+  <div>
+    <Breadcrumb />
+    <Divider horizontal>Settings</Divider>
+    <OperationEditor />
+    <Divider horizontal>Result</Divider>
+    <OperationResult />
+  </div>
+)
+const NotFound = () => (
+  <div>
+    <Breadcrumb />
+    <Divider horizontal>Settings</Divider>
+    <OperationEditor />
+    <Divider horizontal>Result</Divider>
+    <OperationResult />
+  </div>
+)
 
 type Props = {
   mustLogin: boolean
 }
 
-class App extends Component<Props> {
-  render() {
-    return (
-      <SemanticSidebar.Pushable as={Segment} className="no-border">
-        <LoginModal open={this.props.mustLogin} />
-        <Sidebar />
-        <SemanticSidebar.Pusher style={{ maxWidth: window.outerWidth - 260 - 20 }}>
-          <Segment basic className="no-border">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/users/:functional' component={Users} />
-              <Route path='/nonUsers/:functional' component={NonUsers} />
-              <Route path='/customQuery/:functional' component={CustomQuery} />
-              <Route path='/customCommand/:functional' component={CustomCommand} />
-              <Route component={NotFound} />
-            </Switch>
-          </Segment>
-        </SemanticSidebar.Pusher>
-      </SemanticSidebar.Pushable>
-    )
-  }
-}
+const App = ({ mustLogin }: Props) => (
+  <SemanticSidebar.Pushable as={Segment} className="no-border">
+    <LoginModal open={mustLogin} />
+    <Sidebar />
+    <SemanticSidebar.Pusher style={{ maxWidth: window.outerWidth - 260 - 20 }}>
+      <Segment basic className="no-border">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/users/:functional' component={Users} />
+          <Route path='/nonUsers/:functional' component={NonUsers} />
+          <Route path='/customQuery/:functional' component={CustomQuery} />
+          <Route path='/customCommand/:functional' component={CustomCommand} />
+          <Route component={NotFound} />
+        </Switch>
+      </Segment>
+    </SemanticSidebar.Pusher>
+  </SemanticSidebar.Pushable>
+)
 
 const mapStateToProps = (state) => ({
   mustLogin: !state.user.anonymous && !state.user.session
