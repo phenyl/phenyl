c559465fd3081d31abb6227ca16b8a527ae36ceb
Leko <leko.noor@gmail.com>
2018-08-08 14:40:44 +0900

    shallowMap allows obj is null

--- modules/phenyl-api-explorer/src/utils.js
+++ modules/phenyl-api-explorer/src/utils.js
@@ -1,6 +1,10 @@
 // @flow
 
-export function shallowMap(obj: Object, fn: (any, string, Object) => any): Object {
+export function shallowMap(obj: ?Object, fn: (any, string, Object) => any): Object {
+  if (!obj) {
+    return {}
+  }
+
   const ret = {}
   for (let p in obj) {
     ret[p] = fn(obj[p], p, obj)
