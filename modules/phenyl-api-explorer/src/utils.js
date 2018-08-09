e71f6c6b79b70ace361a7ba7000c713112e325c0
Leko <leko.noor@gmail.com>
2018-08-09 14:19:17 +0900

    Add pkgDir to utils

--- modules/phenyl-api-explorer/src/utils.js
+++ modules/phenyl-api-explorer/src/utils.js
@@ -1,4 +1,6 @@
 // @flow
+import fs from 'fs'
+import path from 'path'
 
 export function shallowMap(obj: ?Object, fn: (any, string, Object) => any): Object {
   if (!obj) {
@@ -10,4 +12,14 @@ export function shallowMap(obj: ?Object, fn: (any, string, Object) => any): Obje
     ret[p] = fn(obj[p], p, obj)
   }
   return ret
+}
+
+export const pkgDir = (name: string): ?string => {
+  const dir = require.resolve.paths(name)
+    .find(p => fs.existsSync(path.join(p, name, 'package.json')))
+  if (!dir) {
+    return null
+  }
+
+  return path.join(dir, name)
 }
