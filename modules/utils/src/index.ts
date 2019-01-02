98085c8a369a7c4097ac70aee4aaa8e3f228a5b0
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-01-02 22:18:13 +0900

    revert(utils): Remove normalizeQueryCondition() from index

--- modules/utils/src/index.ts
+++ modules/utils/src/index.ts
@@ -1,8 +1,17 @@
-export * from './request-assertion.js'
-export { createError, createServerError, createLocalError } from './create-error.js'
-export { normalizeQueryCondition } from './normalize-query-condition.js'
-export { PhenylRestApiClient } from './phenyl-rest-api-client.js'
-export { PhenylRestApiDirectClient } from './direct-client.js'
-export { switchByRequestMethod } from './switch-by-request-method.js'
-export { randomString, randomStringWithTimeStamp, timeStampWithRandomString } from './random-string.js'
-export { visitEntitiesInResponseData } from './visit-entities-in-response-data.js'
+export * from "./request-assertion.js";
+export {
+  createError,
+  createServerError,
+  createLocalError
+} from "./create-error.js";
+export { PhenylRestApiClient } from "./phenyl-rest-api-client.js";
+export { PhenylRestApiDirectClient } from "./direct-client.js";
+export { switchByRequestMethod } from "./switch-by-request-method.js";
+export {
+  randomString,
+  randomStringWithTimeStamp,
+  timeStampWithRandomString
+} from "./random-string.js";
+export {
+  visitEntitiesInResponseData
+} from "./visit-entities-in-response-data.js";
