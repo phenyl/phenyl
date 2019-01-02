a436b39363650145a33fd8b342f6c11f743cca61
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-01-02 22:16:01 +0900

    fix(utils): Fix type errors in src/visit-entities-in-response-data.ts

--- modules/utils/src/visit-entities-in-response-data.ts
+++ modules/utils/src/visit-entities-in-response-data.ts
@@ -1,62 +1,70 @@
-import { assign } from 'power-assign/jsnext'
-import { Entity, ResponseData } from 'phenyl-interfaces'
-export type EntityVisitor = (entity: Entity) => Entity
+import { $bind, update } from "@sp2/updater";
+import { Entity, ResponseData } from "@phenyl/interfaces";
+
+export type EntityVisitor = (entity: Entity) => Entity;
+
 /**
  *
  */
-
-export function visitEntitiesInResponseData(resData: ResponseData, visitor: EntityVisitor): ResponseData {
+export function visitEntitiesInResponseData(
+  resData: ResponseData,
+  visitor: EntityVisitor
+): ResponseData {
   switch (resData.type) {
-    case 'get':
-    case 'findOne':
-    case 'insertAndGet':
-    case 'updateAndGet': {
-      const newEntity = visitor(resData.payload.entity)
-      if (newEntity === resData.payload.entity) return resData
-      return assign(resData, {
-        'payload.entity': newEntity,
-      })
+    case "get":
+    case "findOne":
+    case "insertAndGet":
+    case "updateAndGet": {
+      const newEntity = visitor(resData.payload.entity);
+      if (newEntity === resData.payload.entity) return resData;
+      const { $set, $docPath } = $bind<typeof resData>();
+      return update(resData, $set($docPath("payload", "entity"), newEntity));
     }
 
-    case 'getByIds':
-    case 'find':
-    case 'insertAndGetMulti':
-    case 'updateAndFetch': {
-      const newEntities = resData.payload.entities.map(visitor)
-      return assign(resData, {
-        'payload.entities': newEntities,
-      })
+    case "getByIds":
+    case "find":
+    case "insertAndGetMulti":
+    case "updateAndFetch": {
+      const newEntities = resData.payload.entities.map(visitor);
+      const { $set, $docPath } = $bind<typeof resData>();
+      return update(
+        resData,
+        $set($docPath("payload", "entities"), newEntities)
+      );
     }
 
-    case 'pull':
-    case 'push': {
-      if (resData.payload.entity == null) return resData
-      const newEntity = visitor(resData.payload.entity)
-      if (newEntity === resData.payload.entity) return resData
-      return assign(resData, {
-        'payload.entity': newEntity,
-      })
+    case "pull":
+    case "push": {
+      // @ts-ignore
+      if (resData.payload.entity == null) return resData;
+      // @ts-ignore
+      const newEntity = visitor(resData.payload.entity);
+
+      const { $set, $docPath } = $bind<typeof resData>();
+      // @ts-ignore
+      if (newEntity === resData.payload.entity) return resData;
+      // @ts-ignore
+      return update(resData, $set($docPath("payload", "entity"), newEntity));
     }
 
-    case 'login': {
-      if (resData.payload.user == null) return resData
-      const newUser = visitor(resData.payload.user)
-      if (newUser === resData.payload.user) return resData
-      return assign(resData, {
-        'payload.user': newUser,
-      })
+    case "login": {
+      if (resData.payload.user == null) return resData;
+      const newUser = visitor(resData.payload.user);
+      if (newUser === resData.payload.user) return resData;
+      const { $set, $docPath } = $bind<typeof resData>();
+      return update(resData, $set($docPath("payload", "user"), newUser));
     }
 
-    case 'insertOne':
-    case 'insertMulti':
-    case 'updateById':
-    case 'updateMulti':
-    case 'delete':
-    case 'logout': {
-      return resData
+    case "insertOne":
+    case "insertMulti":
+    case "updateById":
+    case "updateMulti":
+    case "delete":
+    case "logout": {
+      return resData;
     }
 
     default:
-      return resData
+      return resData;
   }
 }
