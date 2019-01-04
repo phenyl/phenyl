a33ebbc603ad9ab1e992e7b70b88c104637f9504
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-01-05 01:09:47 +0900

    test(interfaces): Add system for testing type

---
+++ modules/interfaces/type-test/helpers/sample-type-map.ts
@@ -0,0 +1,28 @@
+import { GeneralTypeMap } from "../../src";
+
+export interface SampleTypeMap extends GeneralTypeMap {
+  entities: {
+    member: { id: string; name: string };
+    message: {
+      id: string;
+      body: string;
+    };
+  };
+  customQueries: {
+    countMessagesOfMember: {
+      params: { memberId: string };
+      result: { count: number };
+    };
+    getCurrentVersion: {
+      result: { version: string };
+    };
+  };
+  customCommands: {
+    register: { params: { name: string }; result: { ok: 1 } };
+  };
+  auths: {
+    member: {
+      credentials: { email: string; password: string };
+    };
+  };
+}
