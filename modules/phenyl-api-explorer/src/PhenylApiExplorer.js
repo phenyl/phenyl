a04cfba3fde74d89b8effc532ed3bfd2b0ba9b67
yamatatsu <the.kinnikuman@gmail.com>
2019-02-01 12:17:26 +0900

    explorer: set phenylApiUrlBase to clientside global

--- modules/phenyl-api-explorer/src/PhenylApiExplorer.js
+++ modules/phenyl-api-explorer/src/PhenylApiExplorer.js
@@ -11,17 +11,20 @@ import { shallowMap, pkgDir } from './utils'
 
 export type ExplorerParams = {|
   path: string,
+  phenylApiUrlBase: string,
 |}
 
 export default class PhenylApiExplorer {
   functionalGroup: FunctionalGroup;
   path: string;
+  phenylApiUrlBase: string;
   handler: any;
   html: string;
 
   constructor (functionalGroup: FunctionalGroup, params: ExplorerParams) {
     this.functionalGroup = functionalGroup
     this.path = params.path
+    this.phenylApiUrlBase = params.phenylApiUrlBase || ''
     this.handler = this.handler.bind(this)
     this.html = this.getClientHtml()
   }
@@ -30,6 +33,7 @@ export default class PhenylApiExplorer {
     const templatePath = path.join(pkgDir('phenyl-api-explorer-client'), 'build', 'index.html')
     const template = fs.readFileSync(templatePath, 'utf8')
     const data = {
+      phenylApiUrlBase: this.phenylApiUrlBase,
       functionalGroup: {
         users: shallowMap(this.functionalGroup.users, ({ accountPropName, passwordPropName }) => {
           if (!accountPropName || !passwordPropName) {
