10e66d4bc96b587ca3209c9ca41d91358f8645b6
nicholaslee119 <nicholaslee119@gmail.com>
2019-03-02 18:10:52 +0900

    revert type of redux-localstorage

--- modules/api-explorer-client/src/modules/index.ts
+++ modules/api-explorer-client/src/modules/index.ts
@@ -1,5 +1,6 @@
 import { compose, combineReducers, createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
+// @ts-ignore something is wrong with @types/redux-localstorage
 import persistState from "redux-localstorage";
 import { reducer as operation, Operation } from "./operation";
 import { reducer as user, User } from "./user";
