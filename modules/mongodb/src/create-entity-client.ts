0c296c5fb8f17f256447927664f51c4c478743c7
nicholaslee119 <nicholaslee119@gmail.com>
2019-03-05 17:33:18 +0900

    fix type error of create entity client

--- modules/mongodb/src/create-entity-client.ts
+++ modules/mongodb/src/create-entity-client.ts
@@ -1,14 +1,14 @@
-import type { EntityMap } from 'phenyl-interfaces'
+import { EntityMap } from 'phenyl-interfaces'
 import { PhenylEntityClient } from 'phenyl-central-state/jsnext'
-import { PhenylMongoDbClient } from './mongodb-client.js'
-import type { MongoDbConnection } from './connection.js'
-import type { PhenylEntityClientOptions } from 'phenyl-central-state/jsnext'
+import { PhenylMongoDbClient } from './mongodb-client'
+import { MongoDbConnection } from './connection'
+import { PhenylEntityClientOptions } from 'phenyl-central-state/jsnext'
 
-export function createEntityClient<M: EntityMap>(conn: MongoDbConnection, options: PhenylEntityClientOptions<M> = {}): PhenylMongoDbEntityClient<M> {
+export function createEntityClient<M extends EntityMap>(conn: MongoDbConnection, options: PhenylEntityClientOptions<M> = {}): PhenylMongoDbEntityClient<M> {
   return new PhenylMongoDbEntityClient(conn, options)
 }
 
-export class PhenylMongoDbEntityClient<M: EntityMap> extends PhenylEntityClient<M> {
+export class PhenylMongoDbEntityClient<M extends EntityMap> extends PhenylEntityClient<M> {
   dbClient: PhenylMongoDbClient<M>
 
   constructor(conn: MongoDbConnection, options: PhenylEntityClientOptions<M> = {}) {
