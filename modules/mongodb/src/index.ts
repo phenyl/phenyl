d1bcf0f1d3d88c7096524e41297862c845842711
nicholaslee119 <nicholaslee119@gmail.com>
2019-03-05 17:27:09 +0900

    fix type error of index

--- modules/mongodb/src/index.ts
+++ modules/mongodb/src/index.ts
@@ -1,3 +1,3 @@
 export { connect } from './connection.js'
 export { createEntityClient, PhenylMongoDbEntityClient } from './create-entity-client.js'
-export { PhenylMongoDbClient } from './mongodb-client.js'
+export { PhenylMongoDbClient } from './mongodb-client'
