83035233f9bddb707fcc4160c7c04bf45ed611f0
nicholaslee119 <nicholaslee119@gmail.com>
2019-03-13 13:50:27 +0900

    typescritify change-stream

--- modules/mongodb/src/change-stream.ts
+++ modules/mongodb/src/change-stream.ts
@@ -41,6 +41,6 @@ export type ChangeEvent = UpdateChangeEvent | StandardChangeEvent
 
 
 export interface ChangeStream {
-  next(fn: (err: ?Error, evt: ChangeEvent) => any): any,
+  next(fn: (err: Error | null | undefined, evt: ChangeEvent) => any): any,
   close(): any,
 }
