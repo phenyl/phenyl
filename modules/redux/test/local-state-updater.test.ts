8144ea5ed42d30d546d8624bd29ac1c5dc124acf
nicholaslee119 <nicholaslee119@gmail.com>
2019-04-13 16:58:14 +0900

    fix local state updater tests

--- modules/redux/test/local-state-updater.test.ts
+++ modules/redux/test/local-state-updater.test.ts
@@ -1,98 +1,130 @@
 /* eslint-env mocha */
-import assert from 'assert'
-import { LocalStateUpdater } from '../src/local-state-updater'
-import { PhenylReduxModule } from '../src/phenyl-redux-module'
+import assert from "assert";
+import { LocalStateUpdater } from "../src/local-state-updater";
+import { PhenylReduxModule } from "../src/phenyl-redux-module";
 
-describe('LocalStateUpdater', () => {
-  describe('addUnreachedCommits', () => {
-    it('appends to unreachedCommits', () => {
-      const unreachedCommit = {
-        id: 'hoge',
-        entityName: 'foo',
-        commitCount: 1,
-      }
-      const state = PhenylReduxModule.createInitialState()
-      const newState = Object.assign(state, LocalStateUpdater.addUnreachedCommits(state, unreachedCommit))
-
-      assert.deepStrictEqual(state.unreachedCommits, [])
-      assert.deepStrictEqual(newState.unreachedCommits, [unreachedCommit])
-    })
-    it('calculates the commitCount correctly', () => {
-      const unreachedCommit = {
-        entityName: 'foo',
-        id: 'hoge',
-        commitCount: 5,
-      }
-      const state = PhenylReduxModule.createInitialState()
+describe("LocalStateUpdater", () => {
+  describe("addUnreachedCommits", () => {
+    it("appends to unreachedCommits", () => {
+      const unreachedCommits = {
+        id: "hoge",
+        entityName: "foo",
+        commitCount: 1
+      };
+      const state = PhenylReduxModule.createInitialState();
+      assert.deepEqual(
+        LocalStateUpdater.addUnreachedCommits(state, unreachedCommits),
+        { $push: { unreachedCommits } }
+      );
+    });
+    it("calculates the commitCount correctly", () => {
+      const unreachedCommits = {
+        entityName: "foo",
+        id: "hoge",
+        commitCount: 5
+      };
+      const state = PhenylReduxModule.createInitialState();
       state.unreachedCommits = [
-        { entityName: 'foo', id: 'hoge', commitCount: 2 }
-      ]
-      const newState = Object.assign(state, LocalStateUpdater.addUnreachedCommits(state, unreachedCommit))
+        { entityName: "foo", id: "hoge", commitCount: 2 }
+      ];
 
-      assert.deepStrictEqual(newState.unreachedCommits[1], {
-        entityName: 'foo',
-        id: 'hoge',
-        commitCount: 3,
-      })
-    })
-    it('should not Object.assign new commit when the difference between commitCounts is equal to 0', () => {
+      assert.deepEqual(
+        LocalStateUpdater.addUnreachedCommits(state, unreachedCommits),
+        {
+          $push: {
+            unreachedCommits: {
+              entityName: "foo",
+              id: "hoge",
+              commitCount: 3
+            }
+          }
+        }
+      );
+    });
+    it("should not Object.assign new commit when the difference between commitCounts is equal to 0", () => {
       const unreachedCommit = {
-        entityName: 'foo',
-        id: 'hoge',
-        commitCount: 3,
-      }
-      const state = PhenylReduxModule.createInitialState()
+        entityName: "foo",
+        id: "hoge",
+        commitCount: 3
+      };
+      const state = PhenylReduxModule.createInitialState();
       state.unreachedCommits = [
-        { entityName: 'foo', id: 'hoge', commitCount: 1 },
-        { entityName: 'foo', id: 'hoge', commitCount: 2 }
-      ]
-      const newState = Object.assign(state, LocalStateUpdater.addUnreachedCommits(state, unreachedCommit))
+        { entityName: "foo", id: "hoge", commitCount: 1 },
+        { entityName: "foo", id: "hoge", commitCount: 2 }
+      ];
+      const newState = Object.assign(
+        state,
+        LocalStateUpdater.addUnreachedCommits(state, unreachedCommit)
+      );
 
-      assert.deepStrictEqual(newState.unreachedCommits, state.unreachedCommits)
-    })
-    it('should not Object.assign new commit when the difference between commitCounts is invalid', () => {
+      assert.deepStrictEqual(newState.unreachedCommits, state.unreachedCommits);
+    });
+    it("should not Object.assign new commit when the difference between commitCounts is invalid", () => {
       const unreachedCommit = {
-        entityName: 'foo',
-        id: 'hoge',
-        commitCount: 2,
-      }
-      const state = PhenylReduxModule.createInitialState()
+        entityName: "foo",
+        id: "hoge",
+        commitCount: 2
+      };
+      const state = PhenylReduxModule.createInitialState();
       state.unreachedCommits = [
-        { entityName: 'foo', id: 'hoge', commitCount: 1 },
-        { entityName: 'foo', id: 'hoge', commitCount: 2 }
-      ]
-      const newState = Object.assign(state, LocalStateUpdater.addUnreachedCommits(state, unreachedCommit))
+        { entityName: "foo", id: "hoge", commitCount: 1 },
+        { entityName: "foo", id: "hoge", commitCount: 2 }
+      ];
+      const newState = Object.assign(
+        state,
+        LocalStateUpdater.addUnreachedCommits(state, unreachedCommit)
+      );
 
-      assert.deepStrictEqual(newState.unreachedCommits, state.unreachedCommits)
-    })
-  })
-  describe('removeUnreachedCommits', () => {
-    it('removes a commit from unreachedCommits', () => {
-      const unreachedCommit = {
-        id: 'hoge',
-        entityName: 'foo',
-        commitCount: 1,
-      }
-      const initialState = PhenylReduxModule.createInitialState()
-      const state = Object.assign(initialState, LocalStateUpdater.addUnreachedCommits(initialState, unreachedCommit))
-      const newState = Object.assign(state, LocalStateUpdater.removeUnreachedCommits(state, unreachedCommit))
+      assert.deepStrictEqual(newState.unreachedCommits, state.unreachedCommits);
+    });
+  });
+  describe("removeUnreachedCommits", () => {
+    it("removes a commit from unreachedCommits", () => {
+      const unreachedCommits = {
+        id: "hoge",
+        entityName: "foo",
+        commitCount: 1
+      };
+      const initialState = PhenylReduxModule.createInitialState();
+      const state = Object.assign(
+        initialState,
+        LocalStateUpdater.addUnreachedCommits(initialState, unreachedCommits)
+      );
 
-      assert.deepStrictEqual(state.unreachedCommits, [unreachedCommit])
-      assert.deepStrictEqual(newState.unreachedCommits, [])
-    })
-  })
-  describe('resolveError', () => {
-    it('unsets an error', () => {
-      const state = PhenylReduxModule.createInitialState()
+      assert.deepEqual(
+        LocalStateUpdater.addUnreachedCommits(initialState, unreachedCommits),
+        { $push: { unreachedCommits } }
+      );
+      assert.deepEqual(
+        LocalStateUpdater.removeUnreachedCommits(state, unreachedCommits),
+        {
+          $pull: {
+            unreachedCommits: {
+              $in: [
+                {
+                  commitCount: 1,
+                  entityName: "foo",
+                  id: "hoge"
+                }
+              ]
+            }
+          }
+        }
+      );
+    });
+  });
+  describe("resolveError", () => {
+    it("unsets an error", () => {
+      const state = PhenylReduxModule.createInitialState();
       state.error = {
-        type: 'NetworkFailed',
-        at: 'server',
-        message: 'An error occured',
-        actionTag: 'xxx',
-      }
-      const newState = Object.assign(state, LocalStateUpdater.resolveError())
-
-      assert(typeof newState.error === 'undefined')
-    })
-  })
-})
+        type: "NetworkFailed",
+        at: "server",
+        message: "An error occured",
+        actionTag: "xxx"
+      };
+      assert.deepEqual(LocalStateUpdater.resolveError(), {
+        $unset: { error: "" }
+      });
+    });
+  });
+});
