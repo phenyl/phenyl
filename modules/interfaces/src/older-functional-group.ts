d93c9538a698e09199a6170786b7ff0d98bf2d17
Daiki Ihara <sasurau4@gmail.com>
2019-04-18 15:57:32 +0900

    fix older-functional-group

--- modules/interfaces/src/older-functional-group.ts
+++ modules/interfaces/src/older-functional-group.ts
@@ -1,18 +1,9 @@
 import {
-  AuthCredentials,
-  AuthSessions,
   ReqRes,
-  ReqResAuthUser,
-  ReqResEntity,
   CustomCommandParams,
   CustomCommandResultValue,
-  CustomQueryParams,
-  CustomQueryResultValue,
-  GeneralAuthCommandMap,
   GeneralCustomCommandMap,
-  GeneralCustomMap,
-  GeneralCustomQueryMap,
-  GeneralReqResEntityMap
+  GeneralCustomMap
 } from "./type-map";
 import { AuthDefinition, UserDefinition } from "./user-definition";
 
@@ -22,45 +13,26 @@ import { Entity } from "./entity";
 import { EntityDefinition } from "./entity-definition";
 import { Key } from "./utils";
 
-export interface OlderEntityDefinition<
-  EN extends string = string,
-  Ebroader extends ReqRes<Entity> = ReqRes<Entity>
-> {
-  authorization?: EntityDefinition<EN, Ebroader>["authorize"];
-  normalization?: EntityDefinition<EN, Ebroader>["normalize"];
-  validation?: EntityDefinition<EN, Ebroader>["validate"];
-  wrapExecution?: EntityDefinition<EN, Ebroader>["wrapExecution"];
+export interface OlderEntityDefinition {
+  authorization?: EntityDefinition["authorize"];
+  normalization?: EntityDefinition["normalize"];
+  validation?: EntityDefinition["validate"];
+  wrapExecution?: EntityDefinition["wrapExecution"];
 }
 
-type OlderEntityDefinitions<M extends GeneralReqResEntityMap> = {
-  [EN in Key<M>]:
-    | OlderEntityDefinition<EN, ReqResEntity<M, EN>>
-    | EntityDefinition<EN, ReqResEntity<M, EN>>
+type OlderEntityDefinitions = {
+  [EntityName: string]: OlderEntityDefinition | EntityDefinition;
 };
 
-export interface OlderCustomQueryDefinition<
-  QN extends string = string,
-  QP extends Object = Object,
-  QR extends Object = Object
-> {
-  authorization?: CustomQueryDefinition<QN, QP, QR>["authorize"];
-  normalization?: CustomQueryDefinition<QN, QP, QR>["normalize"];
-  validation?: CustomQueryDefinition<QN, QP, QR>["validate"];
-  execution: CustomQueryDefinition<QN, QP, QR>["execute"];
+export interface OlderCustomQueryDefinition {
+  authorization?: CustomQueryDefinition["authorize"];
+  normalization?: CustomQueryDefinition["normalize"];
+  validation?: CustomQueryDefinition["validate"];
+  execution: CustomQueryDefinition["execute"];
 }
 
-type OlderCustomQueryDefinitions<QM extends GeneralCustomQueryMap> = {
-  [QN in Key<QM>]:
-    | OlderCustomQueryDefinition<
-        QN,
-        CustomQueryParams<QM, QN>,
-        CustomQueryResultValue<QM, QN>
-      >
-    | CustomQueryDefinition<
-        QN,
-        CustomQueryParams<QM, QN>,
-        CustomQueryResultValue<QM, QN>
-      >
+type OlderCustomQueryDefinitions = {
+  [QueryName: string]: OlderCustomQueryDefinition | CustomQueryDefinition;
 };
 
 export interface OlderCustomCommandDefinition<
@@ -68,10 +40,10 @@ export interface OlderCustomCommandDefinition<
   CP extends Object = Object,
   CR extends Object = Object
 > {
-  authorization?: CustomCommandDefinition<CN, CP, CR>["authorize"];
-  normalization?: CustomCommandDefinition<CN, CP, CR>["normalize"];
-  validation?: CustomCommandDefinition<CN, CP, CR>["validate"];
-  execution: CustomCommandDefinition<CN, CP, CR>["execute"];
+  authorization?: CustomCommandDefinition["authorize"];
+  normalization?: CustomCommandDefinition["normalize"];
+  validation?: CustomCommandDefinition["validate"];
+  execution: CustomCommandDefinition["execute"];
 }
 
 type OlderCustomCommandDefinitions<CM extends GeneralCustomCommandMap> = {
@@ -81,11 +53,7 @@ type OlderCustomCommandDefinitions<CM extends GeneralCustomCommandMap> = {
         CustomCommandParams<CM, CN>,
         CustomCommandResultValue<CM, CN>
       >
-    | CustomCommandDefinition<
-        CN,
-        CustomCommandParams<CM, CN>,
-        CustomCommandResultValue<CM, CN>
-      >
+    | CustomCommandDefinition
 };
 
 export interface OlderUserDefinition<
@@ -93,32 +61,17 @@ export interface OlderUserDefinition<
   Ebroader extends ReqRes<Entity> = ReqRes<Entity>,
   C extends Object = Object,
   S extends Object = Object
-> extends OlderEntityDefinition<EN, Ebroader> {
-  authentication: AuthDefinition<EN, Ebroader, C, S>["authenticate"];
+> extends OlderEntityDefinition {
+  authentication: AuthDefinition["authenticate"];
 }
 
-type OlderUserDefinitions<
-  AM extends GeneralAuthCommandMap,
-  EM extends GeneralReqResEntityMap
-> = {
-  [EN in Key<AM>]:
-    | OlderUserDefinition<
-        EN,
-        ReqResAuthUser<AM, EN, EM>,
-        AuthCredentials<AM, EN>,
-        AuthSessions<AM, EN>
-      >
-    | UserDefinition<
-        EN,
-        ReqResAuthUser<AM, EN, EM>,
-        AuthCredentials<AM, EN>,
-        AuthSessions<AM, EN>
-      >
+type OlderUserDefinitions = {
+  [EntityName: string]: OlderUserDefinition | UserDefinition;
 };
 
 export type OlderFunctionalGroup = Partial<{
-  users: OlderUserDefinitions<GeneralAuthCommandMap, GeneralReqResEntityMap>;
-  nonUsers: OlderEntityDefinitions<GeneralReqResEntityMap>;
-  customQueries: OlderCustomQueryDefinitions<GeneralCustomMap>;
+  users: OlderUserDefinitions;
+  nonUsers: OlderEntityDefinitions;
+  customQueries: OlderCustomQueryDefinitions;
   customCommands: OlderCustomCommandDefinitions<GeneralCustomMap>;
 }>;
