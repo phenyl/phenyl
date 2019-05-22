63a52d922adc9016f781ce68fe7f5c28f2831d09
Daiki Ihara <sasurau4@gmail.com>
2019-05-22 17:48:28 +0900

    add createPhenylClients test for memory-db

---
+++ modules/memory-db/test/create-phenyl-clients.test.ts
@@ -0,0 +1,24 @@
+import { describe, it } from "mocha";
+import { createPhenylClients } from "../src/create-phenyl-clients";
+import assert from "assert";
+
+import { PhenylSessionClient } from "@phenyl/central-state";
+import PhenylRestApi from "@phenyl/rest-api";
+import { PhenylMemoryDbEntityClient } from "../src/create-entity-client";
+
+describe("createPhenylClients", () => {
+  const clients = createPhenylClients();
+  it("should create entityClient and sessionClient", () => {
+    const { entityClient, sessionClient } = clients;
+    assert.strictEqual(
+      entityClient instanceof PhenylMemoryDbEntityClient,
+      true
+    );
+    assert.strictEqual(sessionClient instanceof PhenylSessionClient, true);
+  });
+
+  it("should create phenylRestApi correctly", () => {
+    const phenylRestApi = new PhenylRestApi({}, clients);
+    assert.strictEqual(phenylRestApi instanceof PhenylRestApi, true);
+  });
+});
