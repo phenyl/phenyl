07af31792dcc9049ac483fb8ee8fba7d4e0d412e
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-09-04 14:53:17 +0900

    feat(interfaces): Add extra params to query results and command results

--- modules/interfaces/src/query-result.ts
+++ modules/interfaces/src/query-result.ts
@@ -1,27 +1,49 @@
 import { Entity } from "./entity";
 import { GeneralUpdateOperation } from "sp2";
+import { ObjectMap } from "./utils";
 
-export type CustomQueryResult<QR extends Object> = QR;
+type ExtraResult = ObjectMap;
 
-export type QueryResult<E extends Entity> = {
+export type CustomQueryResultObject = ObjectMap & { extra?: undefined };
+
+export type CustomQueryResult<
+  QR extends CustomQueryResultObject,
+  ER extends ExtraResult = ExtraResult
+> = QR & { extra?: ER };
+
+export type QueryResult<
+  E extends Entity,
+  ER extends ExtraResult = ExtraResult
+> = {
   entities: E[];
   versionsById: { [entityId: string]: string | null };
+  extra?: ER;
 };
 
-export type SingleQueryResult<E extends Entity> = {
+export type SingleQueryResult<
+  E extends Entity,
+  ER extends ExtraResult = ExtraResult
+> = {
   entity: E;
   versionId: string | null;
+  extra?: ER;
 };
 
-export type PullQueryResult<E extends Entity> =
+export type PullQueryResult<
+  E extends Entity,
+  ER extends ExtraResult = ExtraResult
+> =
   | {
       pulled: 1;
       operations: GeneralUpdateOperation[];
       versionId: string | null;
+      extra?: ER;
     }
-  | SingleQueryResult<E>;
+  | SingleQueryResult<E, ER>;
 
-export type GeneralCustomQueryResult = CustomQueryResult<Object>;
+export type GeneralCustomQueryResult = CustomQueryResult<
+  CustomQueryResultObject
+>;
 export type GeneralQueryResult = QueryResult<Entity>;
 export type GeneralSingleQueryResult = SingleQueryResult<Entity>;
 export type GeneralPullQueryResult = PullQueryResult<Entity>;
