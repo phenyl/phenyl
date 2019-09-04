95c6c6893e00d29362e50be254f26d74b2073f9c
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-09-04 14:53:11 +0900

    feat(interfaces): Add extra params to queries and commands

--- modules/interfaces/src/query.ts
+++ modules/interfaces/src/query.ts
@@ -1,40 +1,68 @@
 import { FindOperation, SortNotation } from "sp2";
+import { ObjectMap } from "./utils";
 
-export interface WhereQuery<EN extends string> {
+type ExtraParams = ObjectMap;
+
+export interface WhereQuery<
+  EN extends string,
+  EP extends ExtraParams = ExtraParams
+> {
   entityName: EN;
   where: FindOperation;
   skip?: number;
   limit?: number;
   sort?: SortNotation;
+  extra?: EP;
 }
 
-export interface IdQuery<EN extends string> {
+export interface IdQuery<
+  EN extends string,
+  EP extends ExtraParams = ExtraParams
+> {
   entityName: EN;
   id: string;
+  extra?: EP;
 }
 
-export interface IdsQuery<EN extends string> {
+export interface IdsQuery<
+  EN extends string,
+  EP extends ExtraParams = ExtraParams
+> {
   entityName: EN;
   ids: Array<string>;
+  extra?: EP;
 }
 
-export interface PullQuery<EN extends string> {
+export interface PullQuery<
+  EN extends string,
+  EP extends ExtraParams = ExtraParams
+> {
   entityName: EN;
   id: string;
   versionId: string | null;
+  extra?: EP;
 }
 
-export interface CustomQuery<CQ extends string, QP extends Object> {
+export interface CustomQuery<
+  CQ extends string,
+  QP extends Object,
+  EP extends ExtraParams = ExtraParams
+> {
   name: CQ;
   params: QP;
+  extra?: EP;
 }
 
-export interface WhereQuery<EN extends string> {
+export interface WhereQuery<
+  EN extends string,
+  EP extends ExtraParams = ExtraParams
+> {
   entityName: EN;
   where: FindOperation;
   skip?: number;
   limit?: number;
   sort?: SortNotation;
+  extra?: EP;
 }
 
 export type GeneralIdQuery = IdQuery<string>;
