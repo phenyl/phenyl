46499be9e2497ed4a637c51ea71b9195652bf5ab
imoans <naomins0101@gmail.com>
2021-02-06 18:00:41 +0900

    test: add tests for middlewareHandler

---
+++ modules/redux/test/helper.ts
@@ -0,0 +1,119 @@
+import http from "http";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import PhenylHttpServer from "@phenyl/http-server";
+import PhenylRestApi from "@phenyl/rest-api";
+import PhenylHttpClient from "@phenyl/http-client";
+import {
+  GeneralTypeMap,
+  GeneralAction,
+  LocalState,
+  AuthCommandMapOf,
+  ResponseEntityMapOf,
+} from "../../interfaces";
+import { createEntityClient as createMemoryClient } from "@phenyl/memory-db";
+import { StandardUserDefinition } from "@phenyl/standards";
+import { createRedux } from "../src";
+
+export const PORT = 8080;
+const HTTP_CLIENT_URL = `http://localhost`;
+
+type PlainPatient = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type PatientResponse = PlainPatient;
+export type PatientRequest = PlainPatient;
+
+type MyEntityRestInfoMap = {
+  patient: {
+    request: PatientRequest;
+    response: PatientResponse;
+  };
+};
+
+type Store = {
+  phenyl: LocalState<MyEntityRestInfoMap, AuthCommandMapOf<MyTypeMap>>;
+};
+
+interface MyTypeMap extends GeneralTypeMap {
+  entities: MyEntityRestInfoMap;
+  customQueries: {};
+  customCommands: {};
+  auths: {
+    patient: {
+      credentials: {
+        email: string;
+        password: string;
+      };
+      session: { externalId: string; ttl: number };
+    };
+  };
+}
+
+class PatientDefinition extends StandardUserDefinition {
+  constructor(entityClient: ReturnType<typeof createDbClient>) {
+    super({
+      accountPropName: "email",
+      passwordPropName: "password",
+      entityClient,
+      ttl: 24 * 3600,
+    });
+  }
+
+  authorize() {
+    return Promise.resolve(true);
+  }
+}
+
+export const createHttpClient = (port: number): PhenylHttpClient<MyTypeMap> => {
+  return new PhenylHttpClient({
+    url: `${HTTP_CLIENT_URL}:${port}`,
+  });
+};
+
+export const createPhenylRedux = (client: PhenylHttpClient<MyTypeMap>) => {
+  const { reducer, middleware, actions } = createRedux({
+    client,
+    storeKey: "phenyl",
+  });
+  const store = createStore<Store, GeneralAction, {}, {}>(
+    combineReducers({ phenyl: reducer }),
+    applyMiddleware(middleware)
+  );
+  return { store, actions };
+};
+
+export const createDbClient = () => {
+  return createMemoryClient<ResponseEntityMapOf<MyTypeMap>>();
+};
+
+export const createSessionClient = (
+  entityClient: ReturnType<typeof createDbClient>
+) => {
+  return entityClient.createSessionClient<AuthCommandMapOf<MyTypeMap>>();
+};
+
+export const createServer = (dbClient: ReturnType<typeof createDbClient>) => {
+  const functionalGroup = {
+    customQueries: {},
+    customCommands: {},
+    users: {
+      patient: new PatientDefinition(dbClient),
+    },
+    nonUsers: {},
+  };
+
+  const restApiHandler: PhenylRestApi<MyTypeMap> = new PhenylRestApi<MyTypeMap>(
+    functionalGroup,
+    {
+      entityClient: dbClient,
+    }
+  );
+
+  return new PhenylHttpServer(http.createServer(), {
+    restApiHandler,
+  });
+};
---
+++ modules/redux/test/spec/middleware.test.ts
@@ -0,0 +1,187 @@
+import { it, describe, before, after, beforeEach } from "mocha";
+import assert from "assert";
+import * as sinon from "sinon";
+
+import {
+  createHttpClient,
+  createPhenylRedux,
+  createSessionClient,
+  createServer,
+  createDbClient,
+  PORT,
+} from "../helper";
+import { MiddlewareHandler } from "../../src/middleware";
+
+const dbClient = createDbClient();
+const httpClient = createHttpClient(PORT);
+
+const { store, actions } = createPhenylRedux(httpClient);
+const sessionClient = createSessionClient(dbClient);
+const server = createServer(dbClient);
+
+describe("middlewareHandler", async () => {
+  const plainSession = {
+    entityName: "patient" as const,
+    expiredAt: "",
+    userId: "shinout@example.com",
+    externalId: "",
+    ttl: 12345,
+  };
+
+  const plainPatient = {
+    name: "Shin Suzuki",
+    email: "shinout@example.com",
+    password: "shin123",
+  };
+
+  const preSession = await sessionClient.create(plainSession);
+
+  const middlewareHandler = new MiddlewareHandler(
+    () => store.getState().phenyl,
+    httpClient,
+    store.dispatch
+  );
+
+  let insertedPatient: any;
+
+  before(async () => {
+    server.listen(PORT);
+    insertedPatient = await httpClient.insertAndGet(
+      {
+        entityName: "patient",
+        value: plainPatient,
+      },
+      preSession.id
+    );
+    store.dispatch(
+      actions.follow(
+        "patient",
+        insertedPatient.entity,
+        insertedPatient.versionId
+      )
+    );
+  });
+  after(() => {
+    server.close();
+  });
+  describe("unreachedCommits", () => {
+    beforeEach(() => {
+      store.dispatch(
+        actions.assign([
+          {
+            $set: {
+              unreachedCommits: [
+                {
+                  entityName: "patient" as const,
+                  id: insertedPatient.entity.id,
+                  commitCount: 2,
+                },
+                {
+                  entityName: "staff" as const,
+                  id: "",
+                  commitCount: 2,
+                },
+              ],
+            },
+          },
+        ])
+      );
+    });
+
+    describe("commitAndPush", () => {
+      it("clear unreached commits if commits are pushed successfully", async () => {
+        const operations = await middlewareHandler.commitAndPush({
+          type: "phenyl/commitAndPush",
+          payload: {
+            entityName: "patient",
+            id: insertedPatient.entity.id,
+            operation: {},
+          },
+          tag: "",
+        });
+
+        store.dispatch(operations);
+        const { unreachedCommits } = store.getState().phenyl;
+
+        assert.strictEqual(unreachedCommits.length, 1);
+        assert.strictEqual(
+          unreachedCommits.every(({ id }) => id !== insertedPatient.entity.id),
+          true
+        );
+      });
+      it("doesn't clear unreached commits if commits are not pushed", async () => {
+        const stub = sinon.stub(httpClient, "push");
+        stub.throws(() => ({
+          type: "NetworkFailed",
+        }));
+
+        const operations = await middlewareHandler.commitAndPush({
+          type: "phenyl/commitAndPush",
+          payload: {
+            entityName: "patient",
+            id: insertedPatient.entity.id,
+            operation: {},
+          },
+          tag: "",
+        });
+
+        store.dispatch(operations);
+        const { unreachedCommits } = store.getState().phenyl;
+
+        assert.strictEqual(
+          unreachedCommits.filter(({ id }) => id === insertedPatient.entity.id)
+            .length,
+          1
+        );
+        stub.restore();
+      });
+    });
+    describe("pushAndCommit", () => {
+      it("clear unreached commits if commits are pushed successfully", async () => {
+        const operations = await middlewareHandler.pushAndCommit({
+          type: "phenyl/pushAndCommit",
+          payload: {
+            entityName: "patient",
+            id: insertedPatient.entity.id,
+            operation: {},
+          },
+          tag: "",
+        });
+
+        store.dispatch(operations);
+        const { unreachedCommits } = store.getState().phenyl;
+
+        assert.strictEqual(unreachedCommits.length, 1);
+        assert(
+          unreachedCommits.every(({ id }) => id !== insertedPatient.entity.id)
+        );
+      });
+      it("doesn't clear unreached commits if commits are not pushed", async () => {
+        const stub = sinon.stub(httpClient, "push");
+        stub.throws(() => ({
+          type: "NetworkFailed",
+        }));
+
+        const operations = await middlewareHandler.pushAndCommit({
+          type: "phenyl/pushAndCommit",
+          payload: {
+            entityName: "patient",
+            id: insertedPatient.entity.id,
+            operation: {},
+          },
+          tag: "",
+        });
+
+        store.dispatch(operations);
+        const { unreachedCommits } = store.getState().phenyl;
+
+        assert.strictEqual(
+          unreachedCommits.filter(({ id }) => id === insertedPatient.entity.id)
+            .length,
+          1
+        );
+        stub.restore();
+      });
+    });
+  });
+});
