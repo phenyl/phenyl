6710db7f82e2fd9f187f2d6ffff758b4256a501f
imoans <naomins0101@gmail.com>
2021-06-18 13:22:20 +0900

    [api-explorer] fix example

--- modules/interfaces/src/extra.ts
+++ modules/interfaces/src/extra.ts
@@ -3,6 +3,6 @@ import { ObjectMap } from "./utils";
 export type ExtraParams = ObjectMap;
 export type ExtraResult = ObjectMap;
 
-export type CustomResultObject = ObjectMap & { extra?: undefined };
+export type CustomResultObject = ObjectMap & { extra?: ExtraParams };
 export type CustomQueryResultObject = CustomResultObject;
 export type CustomCommandResultObject = CustomResultObject;
